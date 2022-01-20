a2c5b31fa79c4fd3c2f83f51c075f302eb7fb6b7
Mike Carvajal <mike.carvajal.carvajal@intel.com>
2022-01-20 11:18:05 -0600

    Sesion 1/20/22 Cap. 3.21

---
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,29 @@
+interface Producto{
+    desc: string,
+    precio: number
+}
+
+const telNokia: Producto = {
+    desc : 'Nokia A1',
+    precio: 150
+}
+const iphone: Producto = {
+    desc : 'Iphone 12',
+    precio: 350
+}
+ function calcularIVA(productos: Producto[]): [number,number]{
+    let total = 0;
+
+    productos.forEach( ({precio}) => {
+        total+= precio;
+    });
+
+    return [total, total * 0.13];
+
+ }
+
+const articulos: Producto[] = [telNokia,iphone]; 
+const [total,iva] = calcularIVA(articulos);
+
+console.log('IVA: ', iva);
+console.log('Total: ', total);
